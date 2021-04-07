91a08b75f17972c412dd7281bfa2d7e72aaede3c
lucasrocharodrigues <lucasrocha10111997@gmail.com>
2021-04-07 09:12:29 -0300

    bot imposivel de ganhar porem cheio de if

---
+++ scripts/teste.js
@@ -0,0 +1,15 @@
+// cenario que meu bot começa
+
+// quando começar tem que ser sempre pelo [4] ok
+
+// se a segunda jogada for no [1] || [3] || [5] || [7] jogar no [0] || [2] || [6] || [8] 
+
+// cenario que meu bot não começa
+
+// se começou pelo [4] jogar no [0] || [2] || [6] || [8] ok
+
+// se começou pelo [1] || [3] || [5] || [7] || [0] || [2] || [6] || [8] jogar no [4] ok
+
+
+// se tiver dois iguais colocar no 3 de preferencia se for mymove
+//se não tiver 2 colocar na ponta que ja tenha 1
